67b2d1f1572602fb7d7a12c453378f0f4a4f1d71
eyubz <eyerusalembezu15@gmail.com>
2024-12-22 14:35:20 +0300

    Popular event section added on home page

--- Frontend/EventSphere/components/eventCard.js
+++ Frontend/EventSphere/components/eventCard.js
@@ -2,8 +2,12 @@ import { View, Text, Image } from "react-native";
 
 const EventCard = ({ image, name, date, location, time, participant }) => {
   return (
-    <View className="bg-white rounded-lg shadow-lg p-4 mx-4 my-2">
-      <Image source={image} className="w-16 h-32 rounded-lg mb-4" />
+    <View className="rounded-lg shadow-lg p-4 mx-4 my-2 w-fit">
+      <Image
+        source={image}
+        className="w-16 h-32 rounded-lg mb-4"
+        style={{ width: 200, height: 200 }}
+      />
       <Text className="text-xl font-semibold text-gray-800 mb-2">{name}</Text>
       <Text className="text-sm text-gray-600">{date}</Text>
       <Text className="text-sm text-gray-600">{location}</Text>
