79f0a6c7e4f3ee8cdf6a652e3aee95aa03aeac39
eyubz <eyerusalembezu15@gmail.com>
2025-01-08 19:01:33 +0300

    Files updated.

--- Frontend/EventSphere/components/eventCard.js
+++ Frontend/EventSphere/components/eventCard.js
@@ -1,33 +1,31 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image } from "react-native";
 
-const EventCard = ({ image, name, date, location, time, onPress }) => {
+const EventCard = ({ image, name, date, location, time }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View className="bg-white rounded-xl shadow-lg border border-gray-200 m-2 overflow-hidden mr-2 mb-2">
-        <Image
-          source={image}
-          style={{ width: "100%", height: 180 }}
-          className="object-cover"
-        />
+    <View className="bg-white rounded-xl shadow-lg border border-gray-200 m-2 overflow-hidden mr-2 mb-2">
+      <Image
+        source={image}
+        style={{ width: "100%", height: 180 }}
+        className="object-cover"
+      />
 
-        <View className="p-4 space-y-3">
-          <Text className="text-xl font-bold text-primaryPurple truncate">
-            {name}
+      <View className="p-4 space-y-3">
+        <Text className="text-xl font-bold text-primaryPurple truncate">
+          {name}
+        </Text>
+
+        <View className="flex-row justify-between items-center">
+          <Text className="text-sm text-gray-600">📅 {date}</Text>
+          <Text className="text-sm text-gray-600 ml-2">⏰ {time}</Text>
+        </View>
+
+        <View className="flex-row items-start">
+          <Text className="text-sm text-gray-700 ml-1 flex-1">
+            📍 {location}
           </Text>
-
-          <View className="flex-row justify-between items-center">
-            <Text className="text-sm text-gray-600">📅 {date}</Text>
-            <Text className="text-sm text-gray-600 ml-2">⏰ {time}</Text>
-          </View>
-
-          <View className="flex-row items-start">
-            <Text className="text-sm text-gray-700 ml-1 flex-1">
-              📍 {location}
-            </Text>
-          </View>
         </View>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 };
 
--- Frontend/EventSphere/components/filterSection.js
+++ Frontend/EventSphere/components/filterSection.js
@@ -1,11 +1,11 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import Filter from "./filter";
-const filters = ["Nearby", "Recent", "Free", "Paid"];
+const filters = ["Near", "Now", "Free", "Paid"];
 const FilterSection = () => {
   return (
     <View className="flex flex-row gap-4 justify-center items-center">
       {filters.map((filter, index) => (
-        <TouchableOpacity className="bg-primaryPurple rounded-full p-2 mr-4">
+        <TouchableOpacity className="bg-primaryPurple rounded-full mr-4 flex-1 justify-center items-center p-2 w-fit">
           <Filter filter={filter} key={index} />
         </TouchableOpacity>
       ))}
